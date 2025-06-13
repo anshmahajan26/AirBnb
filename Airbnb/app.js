cce951418af61cbaf58655297ce51f21f9eaa6f3
anshmahajan26 <anshmahajan678@gmail.com>
2025-06-13 14:28:25 +0530

    bad chagne

---
+++ Airbnb/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+
+const MONGO_URL = "mongodb+srv://anshmahajan345:grr20QFLPXJqtua2@cluster0.ybos7el.mongodb.net/test?retryWrites=true&w=majority";
+
+async function main() {
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("✅ MongoDB Connected Successfully!");
+  } catch (err) {
+    console.error("❌ MongoDB Connection Failed:", err);
+  }
+}
+main();
+
+app.get("/", (req, res) => {
+  res.send("Hi, I am root");
+});
+
+app.listen(8080, () => {
+  console.log("🚀 Server is listening on port 8080");
+});
